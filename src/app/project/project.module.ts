7631361b135377c4586f8057eae5013e22b81061
Viplav <viplavsoni3333@gmail.com>
2022-04-17 13:36:31 +0530

    Implemented the reducer

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -6,6 +6,7 @@ import { ProjectComponent } from './project.component';
 import { StoreModule } from '@ngrx/store';
 import * as fromProjectModule from './reducers';
 import { ProjectRoutingModule } from './project-routing.module';
+import { proReducer } from './reducers';
 
 
 
@@ -17,7 +18,7 @@ import { ProjectRoutingModule } from './project-routing.module';
     CommonModule,
     CoreModule,
     ProjectRoutingModule,
-    StoreModule.forFeature('project', fromProjectModule.reducers)
+    StoreModule.forFeature('project', proReducer)
   ],
   exports: [
     ProjectComponent
--- src/app/project/reducers/index.ts
+++ src/app/project/reducers/index.ts
@@ -1,17 +1,33 @@
+import { state } from '@angular/animations';
 import {
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
+  createReducer,
   createSelector,
-  MetaReducer
+  MetaReducer,
+  on
 } from '@ngrx/store';
+import { Projects } from 'src/app/model/projects.model';
+import { ProjectAction } from '../action-types';
 
 export const projectModuleFeatureKey = 'projectModule';
 
-export interface State {
-
+export interface ProjectState {
+  project: Projects
 }
 
-export const reducers: ActionReducerMap<State> = {
+export const initialProjectState: ProjectState = {
+  project: new Projects
+}
 
-};
+export const proReducer = createReducer(
+  initialProjectState,
+
+  on(ProjectAction.project, (state, action) => {
+    console.log("logging into the reducer");
+    return {
+      project: action.projects
+    }
+  })
+)
